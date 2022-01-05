2fb451662ac8fb770251d9beb583bbb91f1125a2
Ashley Hodge <ashhodge@comcast.net>
2022-01-04 21:58:25 -0500

    inital commit

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+// Assignment code here
+
+// Create arrays
+var upper = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9","0"]
+var symbol = ["!", "@" ,"#", "$", "%", "^", "&", "*", "_", "+", "-", "~"]
+
+function generatePassword () {
+  var pass = []
+var length = prompt ("How long would you like your password? (must be between 8 - 128 characters)")
+console.log (length) 
+var uppercase = confirm ( "Do you want to use uppercase letters in this password?")
+console.log (uppercase)
+if (uppercase) {
+  
+}
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
